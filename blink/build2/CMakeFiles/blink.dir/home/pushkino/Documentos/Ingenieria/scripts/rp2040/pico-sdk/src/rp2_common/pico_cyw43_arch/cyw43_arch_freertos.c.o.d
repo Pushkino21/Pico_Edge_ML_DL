45a66a5b093118fc13577338af126a6015b8eb48
Electronix_Zealot <nichtmeher@gmail.com>
2026-04-21 21:40:51 -0600

    Removed build folders

--- blink/build2/CMakeFiles/blink.dir/home/pushkino/Documentos/Ingenieria/scripts/rp2040/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/blink.dir/home/pushkino/Documentos/Ingenieria/scripts/rp2040/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c.o: \
- /home/pushkino/Documentos/Ingenieria/scripts/rp2040/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c
--- display_2_test/build2/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/pushkino/Documentos/rp2040/display_2_test/build2/_deps/picotool/"
-};
